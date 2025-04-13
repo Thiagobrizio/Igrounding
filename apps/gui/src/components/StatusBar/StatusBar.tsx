a3e49a6b72c54ba65cf38600199ffa5a39908f45
Thiago Brizio <sota.brizio@gmail.com>
2025-04-13 21:25:10 +1000

    second commit

--- apps/gui/src/components/StatusBar/StatusBar.tsx
+++ apps/gui/src/components/StatusBar/StatusBar.tsx
@@ -1,4 +1,3 @@
-import { Github, Linkedin } from "lucide-react";
 
 import trpc from "~/utils/trpc";
 
@@ -9,20 +8,7 @@ export default function StatusBar() {
         <div className="flex justify-between items-center gap-4 px-4 py-2 border-t bg-background">
             <p className="flex items-center gap-2 text-sm text-muted-foreground">
                 © {new Date().getFullYear()}
-                <a
-                    href="https://github.com/Michaelp1994/transmission-lines"
-                    rel="noreferrer"
-                    target="_blank"
-                >
-                    <Github />
-                </a>
-                <a
-                    href="https://www.linkedin.com/in/michael-poulgrain/"
-                    rel="noreferrer"
-                    target="_blank"
-                >
-                    <Linkedin />
-                </a>
+
             </p>
             {currentProject ? (
                 <div className="flex items-center gap-2">
